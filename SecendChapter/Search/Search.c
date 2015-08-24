75e7896a4d1f923f98bc3486a8251ade891f4020
黒澤 預生 <yonamachan3@gmail.com>
2015-08-25 02:07:32 +0900

    Search コメント記載

--- SecendChapter/Search/Search.c
+++ SecendChapter/Search/Search.c
@@ -1,5 +1,10 @@
 #include "Search.h"
 
+//リニアサーチ(線形探索)
+//これもSortの時と同じで第二,第三引数の意味は配列の長さと配列の先頭アドレスが格納されたポインタを受け取るということを意識
+//第一引数はサーチの対象
+//配列としてのarrayを前から見て行ってdataと一致した時点でその位置(添え字)を返す
+//最後まで見つからなければNOT_FOUNDを返す
 int LinearSearch(int data, int num, int *array) {
   int i;
   int *p;
